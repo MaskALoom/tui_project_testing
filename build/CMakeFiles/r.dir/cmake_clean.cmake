d1c8f17bb867b1b9d722ddf788cfc6b92bee4c09
MaskALoom <maskaloom@gmail.com>
2026-08-22 10:57:08 -0600

    making cmake and fixing compile stuff

---
+++ build/CMakeFiles/r.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/r.dir/src/main.c.o"
+  "CMakeFiles/r.dir/src/main.c.o.d"
+  "r"
+  "r.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/r.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
